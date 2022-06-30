0721e9acaaf58797eb17162c7785c76d77084491
Emioma collins <emiomacollins96@gmail.com>
2022-06-30 13:35:06 -0400

    fix

--- pages/home/api.ts
+++ pages/home/api.ts
@@ -1,7 +1,7 @@
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../../lib/firebase';
 
-export async function fetchFeedbacks() {
+export default async function fetchFeedbacks() {
 	const ref = collection(db, `feedbacks`);
 	const snapshot = await getDocs(ref);
 	const feedbacks = snapshot.docs.map((doc) => doc.data());
