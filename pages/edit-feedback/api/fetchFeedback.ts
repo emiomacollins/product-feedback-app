257c5b721e5b412c2227f6d640ce3a99d3969ef4
Emioma collins <emiomacollins96@gmail.com>
2022-07-05 18:20:34 -0400

    setup

--- pages/edit-feedback/api/fetchFeedback.ts
+++ pages/edit-feedback/api/fetchFeedback.ts
@@ -2,7 +2,7 @@ import { doc, getDoc } from 'firebase/firestore';
 import { db } from '../../../lib/firebase';
 import { Feedback } from '../../../types/feedback';
 
-export async function fetchFeedback(feedbackId: string) {
+export default async function fetchFeedback(feedbackId: string) {
 	const feedbackRef = doc(db, `feedbacks/${feedbackId}`);
 	const snapshot = await getDoc(feedbackRef);
 	return { id: snapshot.id, ...snapshot.data() } as Feedback;
