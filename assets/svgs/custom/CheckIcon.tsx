fcaf3697331e08b8927e40cb52e574b800af6c75
Emioma collins <emiomacollins96@gmail.com>
2022-07-17 22:38:07 -0400

    setup

--- assets/svgs/custom/CheckIcon.tsx
+++ assets/svgs/custom/CheckIcon.tsx
@@ -9,7 +9,7 @@ export default function CheckIcon({ color, ...props }: Props) {
 		<svg xmlns='http://www.w3.org/2000/svg' width='13' height='11' {...props}>
 			<path
 				fill='none'
-				stroke={color ? `var(--${color})` : '#AD1FEA'}
+				stroke={`var(--${color || 'purple'})`}
 				strokeWidth='2'
 				d='M1 5.233L4.522 9 12 1'
 			/>
